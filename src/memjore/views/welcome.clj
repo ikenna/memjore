d5bcf228b472058c1a8653bc27d007c1ea7302ff
Ikenna Nwaiwu <ikenna@memjure.com>
2012-11-12 14:27:17 +0000

    Adding link to all members

--- src/memjore/views/welcome.clj
+++ src/memjore/views/welcome.clj
@@ -2,7 +2,8 @@
   (:require [memjore.views.common :as common]
              [monger core collection])
   (:use [noir.core :only [defpage defpartial]]
-        [hiccup.form :only [label text-field form-to drop-down]]))
+        [hiccup.form :only [label text-field form-to drop-down submit-button text-area]]
+         [hiccup.element :only [link-to]]))
 
 
 (defpage "/" []
@@ -30,9 +31,12 @@
        [:td [:a {:href (str "/member/edit/" (:id member))} "Edit"]] )
 
 (defpage "/members" []
-         (common/layout
-	[:div {:class "center" }
-	   [:table {:border 1}
+  (common/layout
+   [:p (link-to "/member/add" "Add Member")]
+   
+   [:h3 "All Members"]
+   [:div {:class "center" }
+     [:table {:border 1}
 	     [:tr [:th "First Name"] [:th "Last Name"][:th "Mobile"] [:th "Address"][:th ""]]
 	    (for [m (members)]
               [:tr [:td (:fname m)] [:td (:lname m)] [:td (:mobile m)][:td (:addr m)] (edit_button m)]) ]]))
@@ -44,8 +48,9 @@
    [:p   (label "lastname" "Last name:") (text-field "lastname" lname)]
    [:p   (label "mobile" "Mobile:") (text-field "mobile" mobile)]
    [:p   (label "phone no" "Phone number:") (text-field "phone" pnumber)]
-   [:p   (label "address" "Address:") (text-field "address" address) ]
-   [:p   (label "membertype" "Type:")     (drop-down "mtype" mtype)] ] )
+   [:p   (label "address" "Address:") (text-area "address" address) ]
+   [:p   (label "membertype" "Type:")     (drop-down "mtype" mtype)] ]
+   [:p   (submit-button "Submit") ])
 
 
 (defpartial edit-form-heading []
