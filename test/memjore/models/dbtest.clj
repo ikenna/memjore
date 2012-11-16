0ce8a59c9463002472ca3e72fa63a927efb059be
Ikenna Nwaiwu <ikenna@memjure.com>
2012-11-16 18:13:12 +0000

    adding more tests

--- test/memjore/models/dbtest.clj
+++ test/memjore/models/dbtest.clj
@@ -6,10 +6,11 @@
 
 (def john  {:fname "John" :lname "Lewis" :mobile "07838334323" :phone "0268903234" :email "john@localhost.com" :address "No 1 somewhere" :tags "member exco"})
 
-;(fact "Should persist a member"
-; (let [m (add-member john)
-;       id (str (:_id m))]
-;   (:fname (get-member id)) => "John"))
+(fact "Should persist a member"
+      (with-noir
+        (let [m (add-member john)
+              id (str (:_id m))]
+          (:fname (get-member id)) => "John")))
 
 (fact "Should return error message if member to be persisted is invalid"
       (with-noir 
