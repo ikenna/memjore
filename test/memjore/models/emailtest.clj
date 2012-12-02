956a39397b809dc52213a46541f7aad21ed3735f
Ikenna Nwaiwu <ikenna4u@gmail.com>
2012-12-02 22:55:02 +0000

    Removing unneeded files

--- test/memjore/models/emailtest.clj
+++
@@ -1,6 +0,0 @@
-(ns memjore.models.emailtest
-  (:use [midje.sweet]
-   [memjore.models.email]))
-
-(fact "Should send email"
-      1 => 1)
--- test/memjore/models/ta.clj
+++
@@ -1,7 +0,0 @@
-(ns  memjore.models.ta
-  (:use midje.sweet))
-
-(fact "ha"
-  (+ 2 2) => 4
-  (+ 1 1) => 2
-  (+ 2 3) => 5)
